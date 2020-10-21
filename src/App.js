606d70b45bad0de29c8fd9d8db2e920671003799
Haley Myers <haleynmyers@gmail.com>
2020-10-21 11:41:01 -0500

    making routes in App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Wrapper from "./components/Wrapper";
+import Wrapper from "./components/Wrapper/index";
 import Header from "./components/Header/index.js";
 import Section from "./components/Section/index.js";
 import Footer from "./components/Footer/index.js";
+import { Home, Contact, Portfolio, NoMatch } from "./pages";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-   <Wrapper>
-     <Header />
-     <Section />
-     <Footer />
-   </Wrapper>
+    <Router>
+    <Wrapper>
+      <Header />
+        <Section>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contact" component={Contact} />
+            <Route path="*" component={NoMatch} />
+          </Switch>
+        </Section>
+      <Footer />
+    </Wrapper>
+    </Router>
   );
 }
 
