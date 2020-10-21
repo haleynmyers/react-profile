42677ed488f68a27a5b74876fbd5ef7f5eb4765a
Haley Myers <haleynmyers@gmail.com>
2020-10-21 10:02:47 -0500

    put in client directory. built cards

--- src/components/Card.js/index.js
+++ src/components/Card.js/index.js
@@ -1,8 +1,23 @@
-import React, { Component } from "react";
+import React, from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faGitHub } from '@fortawesome/free-solid-svg-icons'
 
 function Card() {
 
-    return ();
+    return (
+    <div className="card" style="width: 18rem;">
+        <div className="card-body">
+            <h5 className="card-title">Contact Me</h5>
+            <p className="card-text"></p>
+        <Link to= activeClassName="active">
+        <FontAwesomeIcon icon={faGitHub}/>
+        GitHub: haleynmyers</Link>
+        <Link to="mailto:haleynmyers@gmail.com" activeClassName="active">
+        <FontAwesomeIcon icon={faEnvelope} />
+        Email: haleynmyers@gmail.com</Link>
+        </div>
+    </div>
+    );
 }
 
 export default Card;
