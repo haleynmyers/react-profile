d575c6f2873adb8f2463790b805a14f1c489d07f
Haley Myers <haleynmyers@gmail.com>
2020-10-23 10:01:44 -0500

    formatting card

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./style.css";
 
 
 function Card(props) {
     return (
       <div className="card" style={{width: "18rem"}}>
-        <h5 className="card-title"> { props.title } </h5>
+        <h3 className="card-header"> { props.title } </h3>
         <div className="card-body">
           {props.children}
         </div>
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,26 +2,40 @@ import React from "react";
 import Card from "../components/Card";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelopeSquare, faExternalLinkSquareAlt, faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
-import { fab, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
 
 function Contact() {
     return(
+<div className="row">
+    <div className="col-md-6 centered">
 
     <Card title="Contact me">
-      <p className="card-text">
-        <FontAwesomeIcon icon={faPhoneSquare} />Cell: 615-315-1593
-        <a href="https://github.com/haleynmyers" activeClassName="active">
-            <FontAwesomeIcon icon={[fab, faGithubSquare]}/>
-            GitHub: haleynmyers</a>
-        <a href="mailto:haleynmyers@gmail.com" activeClassName="active">
-            <FontAwesomeIcon icon={faEnvelopeSquare} />
-            Email: haleynmyers@gmail.com</a>
-        <a href="https://www.linkedin.com/in/haley-myers-56a4a3b5/" activeClassName="active">
-            <FontAwesomeIcon icon={[fab, faExternalLinkSquareAlt]} />
-            LinkedIn: haleynmyers</a>
-      </p>
+      <h5 className="card-text">
+          <ul>
+              <li>
+                <FontAwesomeIcon icon={faPhoneSquare} /> {"  "}Cell: 615-315-1593
+              </li>
+              <li>
+                <a href="https://github.com/haleynmyers" activeClassName="active">
+                <FontAwesomeIcon icon={faGithubSquare}/>{"  "}
+                GitHub: haleynmyers</a>
+              </li>
+              <li>
+                <a href="mailto:haleynmyers@gmail.com" activeClassName="active">
+                    <FontAwesomeIcon icon={faEnvelopeSquare} />{"  "}
+                    Email: haleynmyers@gmail.com</a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/haley-myers-56a4a3b5/" activeClassName="active">
+                <FontAwesomeIcon icon={faExternalLinkSquareAlt} />{"  "}
+                LinkedIn: haleynmyers</a>
+              </li>
+          </ul>
+      </h5>
     </Card>
+    </div>
+</div>
     )
 };
 
